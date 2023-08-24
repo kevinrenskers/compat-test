689201b8718af232bd33bd5915827ddaf187b755
Kevin Renskers <kevin@loopwerk.io>
2023-08-24 16:08:06 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let count = 1;
+</script>
+
+<h1>Welcome to SvelteKit {count}</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<p>
+  <button
+    on:click={() => {
+      count = count + 1;
+    }}>Increase count</button
+  >
+</p>
